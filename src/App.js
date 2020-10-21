30e6e0c324fe6e29bfa6bc0737e6071e065f7ed7
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-21 22:55:15 +0600

    Added Emoji Extention in VS Code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h3>Hello!</h3>
+      <h3>Hello! <span role='img'>👍 😺 ⛄ ⚽ 🔉 🗣️ ❤️</span> </h3>
     </div>
   );
 }
